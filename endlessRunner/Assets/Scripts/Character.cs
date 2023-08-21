aa1639931b080d818c9952e846c924bf62eaa6d8
Taha Temur <tahatemurhd@gmail.com>
2023-08-21 19:46:33 +0300

    update

--- endlessRunner/Assets/Scripts/Character.cs
+++ endlessRunner/Assets/Scripts/Character.cs
@@ -6,7 +6,7 @@ public class Character : MonoBehaviour
 {
     public float velocity = 1f;
     public Rigidbody2D rb2d;
-    public bool isDead= false;
+    public bool isDead;
     
     void Start()
     {
@@ -19,6 +19,14 @@ public class Character : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             rb2d.velocity = Vector2.up * velocity;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="DeathArea")
+        {
+            isDead=true;
+            Time.timeScale = 0;
         }
     }
 }
